1a27c4cd23a05b696b0b310d6533c3e079709c29
Ron Ho <ande1218@gmail.com>
2024-05-31 16:08:34 +0800

    feat: add table

--- src/Views/User.js
+++ src/Views/User.js
@@ -1,10 +1,78 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../contexts/GlobalContext";
+import { Table, Paper } from "../components/common";
+
+const rows = [
+  {
+    _id: "ABC",
+    name: "ABC",
+    identity: "staff",
+    company: "Delta",
+    department: "BASBD",
+    email: "ABC@deltaww.com"
+  },
+  {
+    _id: "DEF",
+    name: "DEF",
+    identity: "staff",
+    company: "Delta",
+    department: "BASBD",
+    email: "DEF@deltaww.com"
+  },
+  {
+    _id: "GHI",
+    name: "GHI",
+    identity: "staff",
+    company: "Delta",
+    department: "BASBD",
+    email: "GHI@deltaww.com"
+  },
+  {
+    _id: "Jay Ciou",
+    name: "Jay Ciou",
+    identity: "VIP",
+    company: "Delta",
+    department: "BASBD",
+    email: "Jay.Ciou@deltaww.com"
+  },
+  {
+    _id: "Uno Lee",
+    name: "Uno Lee",
+    identity: "Guset",
+    company: "",
+    department: "",
+    email: ""
+  },
+  {
+    _id: "UNKNOW",
+    name: "UNKNOW",
+    identity: "Stranger",
+    company: "",
+    department: "",
+    email: ""
+  },
+]
 
 const User = () => {
   const { t } = useContext(GlobalContext);
   return (
-    <h5>{t("current-location", { page: t("user") })}</h5>
+    <Paper>
+      <Table
+        title={t("user")}
+        rows={rows}
+        columns={[
+          { key: 'name', label: t('name') },
+          { key: 'identity', label: t('identity') },
+          { key: 'company', label: t('company') },
+          { key: 'department', label: t('department') },
+          { key: 'email', label: t('email') },
+        ]}
+        orderBy="calories"
+        onPageChange={(event, page) => console.log(page)}
+        onRowsPerPageChange={event => console.log(parseInt(event.target.value, 10))}
+        onSortChange={(isAsc, property) => console.log(isAsc, property)}
+      />
+    </Paper>
   );
 }
 
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -6,3 +6,4 @@ export { default as Paper } from './Paper';
 export { default as Button } from './Button';
 export { default as Switch } from './Switch';
 export { default as TextField } from './TextField';
+export { default as Table } from './Table';
