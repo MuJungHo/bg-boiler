6a3f79d1fd46c8352cd41593fc2a0a65f951fb8b
Ron Ho <ande1218@gmail.com>
2024-06-04 11:09:03 +0800

    Updates

---
+++ precache-manifest.f646f51d23d44f2f53381ac6adc56ef6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f5b4a59112c70eabca7ef6e01b8b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de339e151832c193d4cd",
+    "url": "/static/css/2.378129c6.chunk.css"
+  },
+  {
+    "revision": "68be6cb196b7c2b8f227",
+    "url": "/static/css/main.414b23f7.chunk.css"
+  },
+  {
+    "revision": "de339e151832c193d4cd",
+    "url": "/static/js/2.61729d2e.chunk.js"
+  },
+  {
+    "revision": "0023facb0adb4f9ea5db701618575fa8",
+    "url": "/static/js/2.61729d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68be6cb196b7c2b8f227",
+    "url": "/static/js/main.03de34fa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.03de34fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8c3b90c01e2a4b2e69",
+    "url": "/static/js/runtime-main.007aa73a.js"
+  },
+  {
+    "revision": "dac6fe6f82a8da0f403f76686de066a2",
+    "url": "/static/media/delta.dac6fe6f.svg"
+  }
+]);
--- src/images/icons/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Component } from './Component'
-export { default as DarkMode } from './DarkMode'
-export { default as Delta } from './Delta'
-export { default as Home } from './Home'
-export { default as LightMode } from './LightMode'
-export { default as ManageAccount } from './ManageAccount'
